eb869d7dd62fe0e1ce83bf99252b1e18072398d0
sivaprasad2004 <147930182+sivaprasad2004@users.noreply.github.com>
2024-10-19 13:25:35 +0530

    Add files via upload

---
+++ src/main/java/com/model/Calculator.java
@@ -0,0 +1,23 @@
+package com.model;
+
+
+import javax.ejb.Stateless; 
+ 
+@Stateless 
+public class Calculator implements CalculatorRemote{ 
+ 
+ @Override 
+ public int add(int a, int b) { 
+   
+   
+   
+  return a+b; 
+ } 
+ 
+ @Override 
+ public int sub(int a, int b) { 
+   
+  return a-b; 
+ } 
+ 
+} 
---
+++ src/main/java/com/model/CalculatorRemote.java
@@ -0,0 +1,9 @@
+package com.model; 
+
+public interface CalculatorRemote {
+	
+
+	public int add(int a,int b); 
+	public int sub(int a,int b); 
+	}
+
